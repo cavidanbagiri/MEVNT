7ab9be8f1c9381d75669ff583725614a89435c0a
cavidanbagiri <cavidanbagiri@gmail.com>
2022-12-23 00:58:28 +0300

    Install Cors

--- server/server.js
+++ server/server.js
@@ -5,6 +5,7 @@ const app = express();
 //Dotenv and DB Loaders
 import './config/index.js';
 import './loaders/db.js';
+import cors from 'cors';
 
 
 //Import Routers
@@ -13,6 +14,8 @@ import {UserRouter} from './routes/index.js';
 //User Express Json
 app.use(express.json());
 
+//Use Cors
+app.use(cors());
 
 //Get Routers
 app.use('/user',UserRouter);
