c0cafeeb2562df1325fd57d3673e917addd22e6b
cavidanbagiri <cavidanbagiri@gmail.com>
2023-01-07 18:12:48 +0300

    Adding 404 Not Found Page

--- client/src/router/index.js
+++ client/src/router/index.js
@@ -6,6 +6,7 @@ import {createRouter, createWebHistory} from 'vue-router';
 const HomePage = () => import('../pages/HomePage.vue');
 const ProductItemPage = () => import('../pages/ProductItemPage.vue');
 const CatalogPage = () => import('../pages/CatalogPage.vue');
+const PageNotFound = () => import('../pages/404Page.vue');
 
 const router = createRouter({
 
@@ -14,6 +15,7 @@ const router = createRouter({
         {path:'/', name:'Home', component:HomePage},
         {path:'/product', name:'Product', component:ProductItemPage},
         {path:'/catalog/:category_name', name:'Catalog', component:CatalogPage},
+        { path: '/:pathMatch(.*)*', component: PageNotFound },
     ],
 })
 
