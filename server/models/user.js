424f79f040cc9a4e93189688160462c570b19245
cavidanbagiri <cavidanbagiri@gmail.com>
2023-01-13 22:21:51 +0300

    Changing User Model Default Profile Image

--- server/models/user.js
+++ server/models/user.js
@@ -39,7 +39,8 @@ const userSchema = new mongoose.Schema(
       default: false
     },
     profile_image: {
-      type: String
+      type: String,
+      default: "default.png"
     }
   },
   { timestamps: true }
