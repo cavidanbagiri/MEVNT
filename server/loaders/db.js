76c06bb7d1c65f6cd93624f50a6be85a81304212
cavidanbagiri <cavidanbagiri@gmail.com>
2023-01-26 00:07:39 +0300

    Add Mongo URI

--- server/loaders/db.js
+++ server/loaders/db.js
@@ -1,10 +1,11 @@
 import mongoose from "mongoose";
 
-(async function createConnection () {
+(async function createConnection() {
   mongoose.set("strictQuery", false);
   await mongoose
     .connect(
-      `mongodb://${process.env.DB_HOSTNAME}:${process.env.DB_PORT}/${process.env.DB_NAME}`
+      // `mongodb://${process.env.DB_HOSTNAME}:${process.env.DB_PORT}/${process.env.DB_NAME}`
+      "mongodb+srv://cavidanbagiri:Initial_123@products.5ltfedp.mongodb.net/?retryWrites=true&w=majority"
     )
     .then(() => {
       console.log("Connection Created");
